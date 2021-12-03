99037882102115efed60235972e7323e4ce1f409
ijnek <kenjibrameld@gmail.com>
2021-12-03 12:15:12 +0900

    implemented template class
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

---
+++ include/image_overlay/image_overlay_plugin_t.hpp
@@ -0,0 +1,66 @@
+// Copyright 2021 Kenji Brameld
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IMAGE_OVERLAY__IMAGE_OVERLAY_PLUGIN_T_HPP_
+#define IMAGE_OVERLAY__IMAGE_OVERLAY_PLUGIN_T_HPP_
+
+#include <string>
+#include <memory>
+#include "image_overlay/image_overlay_plugin.hpp"
+#include "rclcpp/typesupport_helpers.hpp"
+#include "rclcpp/serialization.hpp"
+#include "rosidl_runtime_cpp/traits.hpp"
+
+
+template<typename T>
+class ImageOverlayPluginT : public ImageOverlayPlugin
+{
+public:
+  std::string getTopicType() override
+  {
+    return rosidl_generator_traits::name<T>();
+  }
+
+  void overlay(
+    std::shared_ptr<QImage> layer,
+    const std::shared_ptr<rclcpp::SerializedMessage> msg) override
+  {
+    overlay(layer, deserialize(msg));
+  }
+
+  virtual ~ImageOverlayPluginT() {}
+
+protected:
+  ImageOverlayPluginT() {}
+
+  virtual void overlay(
+    std::shared_ptr<QImage> layer,
+    const T msg) = 0;
+
+private:
+  T deserialize(std::shared_ptr<rclcpp::SerializedMessage> msg)
+  {
+    auto library = rclcpp::get_typesupport_library(
+      rosidl_generator_traits::name<T>(), "rosidl_typesupport_cpp");
+    auto string_typesupport = rclcpp::get_typesupport_handle(
+      rosidl_generator_traits::name<T>(), "rosidl_typesupport_cpp", *library);
+    rclcpp::SerializationBase base(string_typesupport);
+
+    T des;
+    base.deserialize_message(msg.get(), &des);
+    return des;
+  }
+};
+
+#endif  // IMAGE_OVERLAY__IMAGE_OVERLAY_PLUGIN_T_HPP_
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['image_overlay'],
     package_dir={'': 'src'},
-    scripts=['scripts/image_overlay']
+    scripts=['scripts/image_overlay'],
 )
 
 setup(**d)
