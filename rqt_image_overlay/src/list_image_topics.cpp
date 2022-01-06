58fd2bdb175f2e58dadeaed87cef8c7bf8c7a6d5
ijnek <kenjibrameld@gmail.com>
2022-01-06 19:24:09 +0900

    simplify list image topics
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- rqt_image_overlay/src/list_image_topics.cpp
+++ rqt_image_overlay/src/list_image_topics.cpp
@@ -15,69 +15,19 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <memory>
 #include "./list_image_topics.hpp"
-#include "image_transport/image_transport.hpp"
 
 namespace rqt_image_overlay
 {
 
 std::vector<std::string> ListImageTopics(const rclcpp::Node & node)
 {
-  std::vector<std::string> message_types{
-    "sensor_msgs/Image",
-    "sensor_msgs/msg/Image"};
-  std::vector<std::string> message_sub_types{
-    "sensor_msgs/CompressedImage",
-    "sensor_msgs/msg/CompressedImage"};
-
-  // get declared transports
-  std::vector<std::string> transports;
-  std::vector<std::string> declared = image_transport::getDeclaredTransports();
-
-  for (std::string transport : declared) {
-    // strip prefix from transport name
-    std::string prefix = "image_transport/";
-
-    size_t index = transport.find(prefix);
-    if (index == 0) {
-      transport = transport.substr(prefix.size());
-    }
-
-    transports.push_back(transport);
-  }
-
-  // Get topics
   std::map<std::string, std::vector<std::string>> topic_info = node.get_topic_names_and_types();
-
-  std::vector<std::string> all_topics;
-  for (auto const & [key, _] : topic_info) {
-    all_topics.push_back(key);
-  }
 
   std::vector<std::string> topics;
   for (auto const & [topic_name, topic_types] : topic_info) {
-    for (const std::string & topic_type : topic_types) {
-      if (std::count(message_types.begin(), message_types.end(), topic_type) > 0) {
-        // add raw topic
-        topics.push_back(topic_name);
-
-        // add transport specific sub-topics
-        for (const std::string & transport : transports) {
-          if (std::count(all_topics.begin(), all_topics.end(), topic_name + "/" + transport) > 0) {
-            std::string sub = topic_name + " " + transport;
-            topics.push_back(sub);
-          }
-        }
-      }
-      if (std::count(message_sub_types.begin(), message_sub_types.end(), topic_type) > 0) {
-        size_t index = topic_name.find_last_of("/");
-        if (index != std::string::npos) {
-          std::string topic_name_copy = topic_name;
-          topic_name_copy.replace(index, 1, " ");
-          topics.push_back(topic_name_copy);
-        }
-      }
+    if (std::count(topic_types.begin(), topic_types.end(), "sensor_msgs/msg/Image") > 0) {
+      topics.push_back(topic_name);
     }
   }
   return topics;
